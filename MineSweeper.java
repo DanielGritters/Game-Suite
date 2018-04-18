c43e954990e35b860d4a3df90e9788730fb535b6
DanielGritters <33299768+DanielGritters@users.noreply.github.com>
2018-04-18 09:04:07 -0400

    Add files via upload

--- MineSweeper.java
+++ MineSweeper.java
@@ -5,19 +5,25 @@ import javax.swing.JFrame;
  * 
  * @author Dan Gritters
  * For CIS350 Final Project - Game Suite
- * runs the minesweeper game by creating a JFrame and attaching the minesweeper panel to it
+ * runs the minesweeper game by creating a JFrame and attaching the 
+ * minesweeper panel to it
  * @version 1.0
  * @since 1.0
  */
+
 public class MineSweeper {
 	
-	public static void main(String[] args){
-		JFrame frame = new JFrame("MineSweeper");
+	/**
+	 * main method.
+	 * @param args arguments
+	 */
+	public static void main(final String[] args) {
+		JFrame frame;
+		frame = new JFrame("MineSweeper");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		MineSweeperPanel panel = new MineSweeperPanel();
 		frame.getContentPane().add(panel);
-		
 		frame.pack();
 		frame.setVisible(true);
 	}
